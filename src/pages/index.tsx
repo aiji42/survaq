0cbaa648f23178e5689902847ee612388e09e2e5
Aiji Uejima <aiji42@gmail.com>
2021-05-08 23:06:10 +0900

    クライアントサイドからプレビューをリクエストできるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import { GetStaticProps } from 'next'
-import { FC } from 'react'
+import { useRouter } from 'next/dist/client/router'
+import { FC, useEffect, useState } from 'react'
 
-const Top: FC = () => {
+interface TopProps {
+  data: any
+}
+
+const Top: FC<TopProps> = ({ data: serverSideData }) => {
+  const [data, setData] = useState(serverSideData)
+  const router = useRouter()
+  useEffect(() => {
+    if (!router.query.preview) return
+    fetch(`/api/preview?draftKey=${router.query.preview}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(() => null)
+  }, [router])
+
+  console.log(data)
+
   return null
 }
 
 export default Top
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<TopProps> = async () => {
   const data = await fetch(`${process.env.MICRO_CMS_API_ENDPOINT}/top`, {
     headers: { 'X-API-KEY': process.env.MICRO_CMS_API_KEY ?? '' }
   })
