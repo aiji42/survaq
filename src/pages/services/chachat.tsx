3c008ad49977e4102312a1710eeb4f2802c8ee89
Aiji Uejima <aiji42@gmail.com>
2021-05-20 23:50:11 +0900

    FAQ

--- src/pages/services/chachat.tsx
+++ src/pages/services/chachat.tsx
@@ -50,14 +50,14 @@ const Chachat: FC = () => {
           </header>
           <main className="flex flex-col-reverse sm:flex-row jusitfy-between items-center py-12">
             <div className="sm:w-2/5 flex flex-col items-center sm:items-start text-center sm:text-left">
-              <h1 className="uppercase text-6xl text-green-400 font-bold leading-none tracking-wide mb-2">
+              <h1 className="text-5xl text-green-400 font-bold leading-none tracking-wide mb-2">
                 チャット型フォームの革命児
               </h1>
-              <h2 className="uppercase text-4xl text-orange-500 text-secondary tracking-widest mb-6">
+              <h2 className="text-3xl text-orange-500 mb-6">
                 導入・運用コストゼロ！
               </h2>
               <p className="text-gray-600 leading-relaxed mb-12">
-                チャチャットは導入がかんたんなチャット型フォームです。対話形式でユーザの情報を獲得することができます。専門的な知識は必要ありません。
+                チャチャットは永久無料で使用できる導入がかんたんなチャット型フォームです。対話形式でユーザの情報を獲得することができます。専門的な知識は必要ありません。
               </p>
               <a
                 href="#"
@@ -159,8 +159,137 @@ const Chachat: FC = () => {
                 どんなに使用しても費用をいただくことはございません。
               </p>
               <p className="text-gray-600 py-2">
-                予告なく価格プランが追加・変更になる可能性がございますが、<br />プランが自動的に切り替わることはございませんので、安心してご利用いただけます。
+                予告なく価格プランが追加・変更になる可能性がございますが、
+                <br />
+                プランが自動的に切り替わることはございませんので、安心してご利用いただけます。
               </p>
+            </div>
+          </section>
+
+          <section className="w-full max-w-5xl mx-auto mt-32">
+            <div className="shadow bg-white">
+              <article className="border-b">
+                <div className="border-l-2 border-transparent">
+                  <header className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none">
+                    <span className="text-grey-darkest font-thin text-xl">
+                      どんなサイトで使用できますか？
+                    </span>
+                    <div className="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
+                      <svg
+                        aria-hidden="true"
+                        className=""
+                        data-reactid="266"
+                        fill="none"
+                        height="24"
+                        stroke="#606F7B"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        viewbox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                      </svg>
+                    </div>
+                    <div>
+                      <p className="px-8 py-3 leading-6 text-gray-600">
+                        <span className="font-bold leading-10">
+                          チャットを起動するための短いHTMLを挿入することができるサイトであれば、どんなサイトでも導入可能です。
+                        </span>
+                        <br />
+                        
+                        <br />
+                        コストの心配をすることなく、無料で使用していただけるツールを提供することが、多くの類似のサービスとの差別化を行う上で一番効果的であると私達は考えました。
+                        <br />
+                        今後、継続的に行われる追加機能の開発において、一部の機能を有料化する可能性がゼロであるとは言えません。
+                        <br />
+                        しかし、通知なくこの方針を変えることはありませんし、費用をあとからご請求することもございませんのでご安心ください。
+                      </p>
+                    </div>
+                  </header>
+                </div>
+              </article>
+              <article className="border-b">
+                <div className="border-l-2 bg-grey-lightest border-blue-400">
+                  <header className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none">
+                    <span className="text-indigo-400 text-xl">
+                      本当に無料で使用できますか？
+                    </span>
+                    <div className="rounded-full border border-indigo-600 w-7 h-7 flex items-center justify-center bg-indigo-400">
+                      <svg
+                        aria-hidden="true"
+                        data-reactid="281"
+                        fill="none"
+                        height="24"
+                        stroke="white"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        viewbox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <polyline points="18 15 12 9 6 15"></polyline>
+                      </svg>
+                    </div>
+                  </header>
+                  <div>
+                    <p className="px-8 py-3 leading-6 text-gray-600">
+                      <span className="font-bold leading-10">
+                        制限なく無料でご使用いただけます。費用を徴収する予定も今のところございません。
+                      </span>
+                      <br />
+                      「え！？こんなに便利な機能を無料で使えるの？」と思っていただけるような開発をしていくことこそが、私達の美学です。
+                      <br />
+                      コストの心配をすることなく、無料で使用していただけるツールを提供することが、多くの類似のサービスとの差別化を行う上で一番効果的であると私達は考えました。
+                      <br />
+                      今後、継続的に行われる追加機能の開発において、一部の機能を有料化する可能性がゼロであるとは言えません。
+                      <br />
+                      しかし、通知なくこの方針を変えることはありませんし、費用をあとからご請求することもございませんのでご安心ください。
+                    </p>
+                  </div>
+                </div>
+              </article>
+              <article className="border-b">
+                <div className="border-l-2 bg-grey-lightest border-blue-400">
+                  <header className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none">
+                    <span className="text-indigo-400 text-xl">
+                      ツールの使い方・分析や改善に関してサポートを受けたいのですが？
+                    </span>
+                    <div className="rounded-full border border-indigo-600 w-7 h-7 flex items-center justify-center bg-indigo-400">
+                      <svg
+                        aria-hidden="true"
+                        data-reactid="281"
+                        fill="none"
+                        height="24"
+                        stroke="white"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        viewbox="0 0 24 24"
+                        width="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <polyline points="18 15 12 9 6 15"></polyline>
+                      </svg>
+                    </div>
+                  </header>
+                  <div>
+                    <p className="px-8 py-3 leading-6 text-gray-600">
+                      <span className="font-bold leading-10">
+                        お手伝いができる範囲で可能な限りサポートさせていただきます。サポート宛のメールアドレスにご連絡ください。support@survaq.com
+                      </span>
+                      <br />
+                      運営が間もなくノウハウが多くあるわけではございませんので、すべてのご要望にお応えできるかはわかりません。
+                      <br />
+                      しかし、お役に立てるように誠心誠意努力をいたします。
+                      <br />
+                      また、お客様からの声や要望をもとに機能改善や、マニュアルの作成などを行ってまいりますので、ご意見ご要望を気軽にお問合せください。
+                    </p>
+                  </div>
+                </div>
+              </article>
             </div>
           </section>
         </div>
