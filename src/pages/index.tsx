e1b9a7df536741670607e3b0632840007ed33cf9
Aiji Uejima <aiji42@gmail.com>
2021-05-23 21:53:22 +0900

    previewの処理を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,20 @@
 import { GetStaticProps } from 'next'
-import { useRouter } from 'next/dist/client/router'
-import { FC, useEffect, useState } from 'react'
-import { Top as TopType } from '../types/top'
+import { client } from '@/libs/microCms'
+import { FC } from 'react'
+import { Top as TopType } from '@/types/top'
 import Head from 'next/head'
-import { Mission } from '../components/Mission'
-import { Bussiness } from '../components/Bussiness'
-import { Products } from '../components/Products'
-import { Company } from '../components/Company'
-import { TopPageHeader } from '../components/TopPageHeader'
-import { Footer } from '../components/Footer'
+import { Mission } from '@/components/Mission'
+import { Bussiness } from '@/components/Bussiness'
+import { Products } from '@/components/Products'
+import { Company } from '@/components/Company'
+import { TopPageHeader } from '@/components/TopPageHeader'
+import { Footer } from '@/components/Footer'
 
-interface TopProps {
+export interface TopProps {
   data: TopType
 }
 
-const Top: FC<TopProps> = ({ data: serverSideData }) => {
-  const [data, setData] = useState(serverSideData)
-  const router = useRouter()
-  useEffect(() => {
-    if (!router.query.preview) return
-    fetch(`/api/preview/top?draftKey=${router.query.preview}`)
-      .then((res) => res.json())
-      .then(setData)
-      .catch(() => null)
-  }, [router])
-
+const Top: FC<TopProps> = ({ data }) => {
   return (
     <>
       <Head>
@@ -48,16 +38,21 @@ const Top: FC<TopProps> = ({ data: serverSideData }) => {
 export default Top
 
 export const getStaticProps: GetStaticProps<TopProps> = async () => {
-  const data = await fetch(`${process.env.MICRO_CMS_API_ENDPOINT}/top`, {
-    headers: { 'X-API-KEY': process.env.MICRO_CMS_API_KEY ?? '' }
-  })
-    .then((res) => res.json())
-    .catch(() => null)
+  try {
+    const data = await client.get<TopType>({
+      endpoint: 'top'
+    })
 
-  return {
-    props: {
-      data
-    },
-    revalidate: 5
+    return {
+      props: {
+        data
+      },
+      revalidate: 5
+    }
+  } catch (e) {
+    console.log(e)
+    return {
+      notFound: true
+    }
   }
 }
