99384b7a48582cca29b2649ca7618352958648d6
Aiji Uejima <aiji42@gmail.com>
2021-05-23 17:38:56 +0900

    フッターのprops変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
-import { Top } from '../types/top'
 
-export const Footer: FC<Top> = ({ meta }) => {
+export const Footer: FC<{ copyright?: string }> = ({ copyright }) => {
   return (
     <footer className="border-t p-4 md:p-8 mx-auto text-center text-gray-500">
       <ul className="max-w-3xl mx-auto flex justify-evenly flex-wrap">
@@ -15,7 +14,7 @@ export const Footer: FC<Top> = ({ meta }) => {
           <a href="mailto:support@survaq.com">お問合せ</a>
         </li>
       </ul>
-      <p className="pt-4">{meta.copyright}</p>
+      {copyright && <p className="pt-4">{copyright}</p>}
     </footer>
   )
 }
