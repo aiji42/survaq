e31936ee3c695813989e4bbf40db81ae3d303110
Aiji Uejima <aiji42@gmail.com>
2021-05-23 17:30:19 +0900

    フッターのスタイル変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react'
+import { Top } from '../types/top'
 
-export const Footer: FC<{ copyright: string }> = ({ copyright }) => {
+export const Footer: FC<Top> = ({ meta }) => {
   return (
-    <footer className="border-t">
-      <div className="py-8 text-center">
-        <p className="text-gray-500">{copyright}</p>
-      </div>
+    <footer className="border-t p-4 md:p-8 mx-auto text-center text-gray-500">
+      <ul className="max-w-3xl mx-auto flex justify-evenly flex-wrap">
+        <li>
+          <a href="/#company">会社情報</a>
+        </li>
+        <li>
+          <a href="/policies/privacy">プライバシーポリシー</a>
+        </li>
+        <li>
+          <a href="mailto:support@survaq.com">お問合せ</a>
+        </li>
+      </ul>
+      <p className="pt-4">{meta.copyright}</p>
     </footer>
   )
 }
