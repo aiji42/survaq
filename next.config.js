6537b4e5d51064ad6966b34c367f93cff04ef27c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-01-27 23:34:00 +0900

    古い情報の削除 & bumpup

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
-const rewrites = async () => {
-  return {
-    fallback: [
-      {
-        source: '/assets/:path*',
-        destination: 'http://survaq-chachat.s3-website-ap-northeast-1.amazonaws.com/assets/:path*'
-      },
-      {
-        source: '/:path*',
-        destination: 'http://survaq-chachat.s3-website-ap-northeast-1.amazonaws.com/:path*/'
-      }
-    ]
-  }
-}
-
 module.exports = {
   images: {
     domains: ['images.microcms-assets.io'],
-  },
-  webpack: {},
-  rewrites
+  }
 }
