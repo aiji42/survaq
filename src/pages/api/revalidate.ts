f7b6418486670d79e24c51a5632ade6a8c248a7b
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-02-21 23:20:57 +0900

    update revalidate route

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,26 +1,26 @@
 import { NextApiHandler, NextApiRequest } from 'next'
 import crypto from 'crypto'
 
-const validate = (request: NextApiRequest): boolean => {
-  const expectedSignature = crypto
-    .createHmac('sha256', process.env.SECRET_KEY ?? '')
-    .update(
-      typeof request.body === 'string'
-        ? request.body
-        : JSON.stringify(request.body)
-    )
-    .digest('hex')
-  const signature = request.headers['x-microcms-signature'] as string
-  if (
-    !crypto.timingSafeEqual(
-      Buffer.from(signature),
-      Buffer.from(expectedSignature)
-    )
-  ) {
-    throw new Error('Invalid signature.')
-  }
-  return true
-}
+// const validate = (request: NextApiRequest): boolean => {
+//   const expectedSignature = crypto
+//     .createHmac('sha256', process.env.SECRET_KEY ?? '')
+//     .update(
+//       typeof request.body === 'string'
+//         ? request.body
+//         : JSON.stringify(request.body)
+//     )
+//     .digest('hex')
+//   const signature = request.headers['x-microcms-signature'] as string
+//   if (
+//     !crypto.timingSafeEqual(
+//       Buffer.from(signature),
+//       Buffer.from(expectedSignature)
+//     )
+//   ) {
+//     throw new Error('Invalid signature.')
+//   }
+//   return true
+// }
 
 const handler: NextApiHandler = async (req, res) => {
   if (!req.body.api || !req.body.id) {
@@ -28,7 +28,6 @@ const handler: NextApiHandler = async (req, res) => {
   }
   console.log(req.body)
   try {
-    validate(req)
     if (req.body.api === 'top') {
       await res.unstable_revalidate('/')
       res.status(200)
