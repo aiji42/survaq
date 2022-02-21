81e0efc22c79f7e99b00982ca0b51b6fd7179750
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-02-21 23:28:24 +0900

    update revalidate route

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,26 +1,26 @@
 import { NextApiHandler, NextApiRequest } from 'next'
 import crypto from 'crypto'
 
-// const validate = (request: NextApiRequest): boolean => {
-//   const expectedSignature = crypto
-//     .createHmac('sha256', process.env.SECRET_KEY ?? '')
-//     .update(
-//       typeof request.body === 'string'
-//         ? request.body
-//         : JSON.stringify(request.body)
-//     )
-//     .digest('hex')
-//   const signature = request.headers['x-microcms-signature'] as string
-//   if (
-//     !crypto.timingSafeEqual(
-//       Buffer.from(signature),
-//       Buffer.from(expectedSignature)
-//     )
-//   ) {
-//     throw new Error('Invalid signature.')
-//   }
-//   return true
-// }
+const validate = (request: NextApiRequest): boolean => {
+  const expectedSignature = crypto
+    .createHmac('sha256', process.env.SECRET_KEY ?? '')
+    .update(
+      typeof request.body === 'string'
+        ? request.body
+        : JSON.stringify(request.body)
+    )
+    .digest('hex')
+  const signature = request.headers['x-microcms-signature'] as string
+  if (
+    !crypto.timingSafeEqual(
+      Buffer.from(signature),
+      Buffer.from(expectedSignature)
+    )
+  ) {
+    throw new Error('Invalid signature.')
+  }
+  return true
+}
 
 const handler: NextApiHandler = async (req, res) => {
   if (!req.body.api || !req.body.id) {
@@ -28,12 +28,13 @@ const handler: NextApiHandler = async (req, res) => {
   }
   console.log(req.body)
   try {
+    validate(req)
     if (req.body.api === 'top') {
-      res.unstable_revalidate('/')
+      await res.unstable_revalidate('/')
       res.status(200).json({})
       return
     }
-    res.unstable_revalidate(`/${req.body.api}/${req.body.id}`)
+    await res.unstable_revalidate(`/${req.body.api}/${req.body.id}`)
     res.status(200).json({})
     return
   } catch (e) {
